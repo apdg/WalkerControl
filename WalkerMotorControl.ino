d550e62b82c10fb81f3deaa0e6aedcb48af2319c
Adam <apdg@uw.edu>
2023-09-04 20:11:56 -0700

    more science notes, testing with higher speeds

--- WalkerMotorControl.ino
+++ WalkerMotorControl.ino
@@ -3,15 +3,16 @@
 int ControlPin = 3;   //give your arduino pin a name
 bool rocking = LOW;
 int switchState = LOW;
-int DutyCyclePercent = 60;
+int DutyCyclePercent = 66;
 long timerStart = 0;
 
 int targetKilometers = 10;
 int estimatedMinutesPerKM = 12;
 int bufferTimeMin = 15;
-long timerDuration = 4600000;
+long timerDuration = 3900000;
 //red: 9m@58 = 6.4km -> 0.71m/s, 7m@57 = 4.7km -> 0.67m/s
-//black: 0.7m@58 = 0.7km -> 1m/s, 2.1m@59 = 2.24km -> 1.07m/s, 4.6m@60 = 
+//black: 0.7m@58 = 0.7km -> 1m/s, 2.1m@59 = 2.24km -> 1.07m/s, 4039589@60 = 4.1k -> 1.01m/s, 4.9m@60 = 5.17km -> 1.055m/s
+// 690sec@66 = 700m -> 1.014m/s, 3.9m@66 = 4.73km -> 1.21m/s
 
 void setup() {
   
@@ -21,21 +22,38 @@ void setup() {
   Serial.begin(9600);          //setup the serial monitor for viewing the active PWM control value
 }
 
+void readouts() {
+    Serial.print("Timer Checkpoint: ");
+    Serial.println(timerStart);
+
+    // Serial.println(targetKilometers);
+    // Serial.println(estimatedMinutesPerKM);
+
+    if (rocking) {
+      Serial.print("Rocking started. Duty cycle: ");
+      Serial.print(DutyCyclePercent);
+      Serial.println("%");
+
+      Serial.print("Timer Delta Setting: ");
+      Serial.print(timerDuration/1000);
+      Serial.println(" seconds.\n");
+    }
+    if (!rocking) {
+      Serial.println("Rocking stopped.\n");
+    }
+
+}
+
 void loop() { 
 
   switchState = digitalRead(2);
 
   if (switchState == HIGH) {
     timerStart = millis();
-    Serial.println(timerStart);
-    Serial.println(targetKilometers);
-    Serial.println(estimatedMinutesPerKM);
-    Serial.println(timerDuration);
-    Serial.print("Duty cycle percent: ");
-    Serial.println(DutyCyclePercent);
     digitalWrite(ControlPin, HIGH);
     delay(340);
     rocking = !rocking;
+    readouts();
   }
   
   if (millis()-timerStart > timerDuration){
