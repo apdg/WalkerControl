3d93666a46161350ed2fc2b033bb572108db04ef
Adam <apdg@uw.edu>
2023-09-17 22:56:22 -0700

    just a 2hr delay on button press

--- WalkerMotorControl.ino
+++ WalkerMotorControl.ino
@@ -9,7 +9,7 @@ long timerStart = 0;
 int targetKilometers = 10;
 int estimatedMinutesPerKM = 12;
 int bufferTimeMin = 15;
-long timerDuration = 2400000;
+long timerDuration = 12000000;
 //red: 9m@58 = 6.4km -> 0.71m/s, 7m@57 = 4.7km -> 0.67m/s
 //black: 0.7m@58 = 0.7km -> 1m/s, 2.1m@59 = 2.24km -> 1.07m/s, 4039589@60 = 4.1k -> 1.01m/s, 4.9m@60 = 5.17km -> 1.055m/s
 // 690sec@66 = 700m -> 1.014m/s, 3.9m@66 = 4.73km -> 1.21m/s, 1.7m@66 = 1.23km, 12m@65 = 8.7km
@@ -19,7 +19,7 @@ void setup() {
   pinMode(ControlPin, OUTPUT); // initialize the digital pin as an output.
   pinMode(2, INPUT);
 
-  Serial.begin(9600);          //setup the serial monitor for viewing the active PWM control value
+  Serial.begin(11520);          //setup the serial monitor for viewing the active PWM control value
 }
 
 void readouts() {
@@ -49,11 +49,15 @@ void loop() {
   switchState = digitalRead(2);
 
   if (switchState == HIGH) {
+    Serial.println("Two hour delay started.");
+    delay(7200000);
+    Serial.println("Two hour delay ended.");
     timerStart = millis();
+    Serial.println("Run timer of " + String(timerDuration/1000) + " seconds started.");
     digitalWrite(ControlPin, HIGH);
     delay(340);
     rocking = !rocking;
-    readouts();
+    // readouts();
   }
   
   if (millis()-timerStart > timerDuration){
