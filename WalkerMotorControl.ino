ca2995c4b792c041a7583efabfdf2703acd6e22a
Adam <apdg@uw.edu>
2023-09-20 22:36:56 -0700

    more file cleanup

--- WalkerMotorControl.ino
+++ WalkerMotorControl.ino
@@ -3,23 +3,27 @@
 int ControlPin = 3;   //give your arduino pin a name
 bool rocking = LOW;
 int switchState = LOW;
-int DutyCyclePercent = 65;
+int DutyCyclePercent = 82;
 long timerStart = 0;
+bool speedChange = LOW;
+bool bedtimeDelay = LOW;
+int timeCheck = 0;
 
 int targetKilometers = 10;
 int estimatedMinutesPerKM = 12;
 int bufferTimeMin = 15;
-long timerDuration = 2400000;
+long timerDuration = 12000000;
 //red: 9m@58 = 6.4km -> 0.71m/s, 7m@57 = 4.7km -> 0.67m/s
 //black: 0.7m@58 = 0.7km -> 1m/s, 2.1m@59 = 2.24km -> 1.07m/s, 4039589@60 = 4.1k -> 1.01m/s, 4.9m@60 = 5.17km -> 1.055m/s
 // 690sec@66 = 700m -> 1.014m/s, 3.9m@66 = 4.73km -> 1.21m/s, 1.7m@66 = 1.23km, 12m@65 = 8.7km
+// 10.3k from 8m ms, 6.8 from 5.5m - looking like 1.2 is reliable on black phone
 
 void setup() {
   
   pinMode(ControlPin, OUTPUT); // initialize the digital pin as an output.
   pinMode(2, INPUT);
 
-  Serial.begin(9600);          //setup the serial monitor for viewing the active PWM control value
+  Serial.begin(115200);          //setup the serial monitor for viewing the active PWM control value
 }
 
 void readouts() {
@@ -44,28 +48,67 @@ void readouts() {
 
 }
 
+void DelayStart() {
+    Serial.println("Two hour delay started.");
+    delay(10000000);
+    Serial.println("Two hour delay ended.");
+}
+
 void loop() { 
 
   switchState = digitalRead(2);
 
   if (switchState == HIGH) {
+    if (bedtimeDelay){
+      DelayStart();
+    }
+    else{
+      Serial.println("Start delay off.");
+    }
     timerStart = millis();
+    timeCheck = 0;
+    Serial.println("Run timer of " + String(timerDuration/1000) + " seconds started.");
     digitalWrite(ControlPin, HIGH);
     delay(340);
     rocking = !rocking;
-    readouts();
+    speedChange = HIGH;
+    //readouts();
   }
   
-  if (millis()-timerStart > timerDuration){
+  if (millis()-timerStart > timerDuration && speedChange == LOW){
+    Serial.println("Run timer ended.");
     rocking = LOW;
+    speedChange = HIGH;
   }
 
-  if (rocking == HIGH) {
+  if (rocking == HIGH && speedChange == HIGH) {
     analogWrite(ControlPin, int(DutyCyclePercent*255/100));
+    speedChange = LOW;
   }
   
-  if (rocking == LOW) {
+  if (rocking == LOW && speedChange == HIGH) {
     digitalWrite(ControlPin, LOW);
+    speedChange = LOW;
   }
+
+  if (round((millis()-timerStart)/1000) % 2 == 0)
+  {
+    timeCheck++;
+    Serial.println("Timer (minutes): " + String(timeCheck));
+  }
+  
 
 }
+
+// constant : watch for button presses
+
+
+
+  // seconds = millis()/1000;
+
+  // if (seconds % 2 == 0){
+  //   digitalWrite(13, HIGH);
+  // }
+  // else{
+  //   digitalWrite(13, LOW);
+  // }
